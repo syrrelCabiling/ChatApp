089041306681d422536455349088c75bec9c82dc
Syrrel Cabiling <syrrelcabiling@Syrrels-MBP.fc.ca>
2020-01-29 08:44:05 -0500

    added socket

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+// add socket.io
+const io = require('socket.io')(); //running function right away
+
 const port = process.env.PORT || 3030;
 
 // tell express where our static files are (js, images, css etc)
@@ -13,3 +16,17 @@ app.get('/', (req, res) => {
 const server = app.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });
+
+
+
+// attach our chat server to our app
+io.attach(server);
+
+io.on('connection', function(socket) {
+    console.log('a user has connected');
+    socket.emit('connected', {sID: socket.id, message: "new connection"});
+
+    socket.on('disconnect', function() {
+        console.log('a user has disconnected');
+    })
+})
--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,1 +1,16 @@
 // imports always go first - if we're importing anything
+
+const socket = io(); //loading from node_modules folder
+
+//the packet is whatever data we send thru with the connect event from the server
+function setUserId(packet) {
+    //debugger;
+    console.log(packet);
+}
+
+function showDisconnect() {
+    console.log('a user disconnected');
+}
+
+socket.addEventListener('connected', setUserId);
+socket.addEventListener('disconnect',showDisconnect);
